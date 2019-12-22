094a02520fbf0fbf6a2b7eb6e6270d9226645a16
Jared Bienz <jbienz@hotmail.com>
2019-12-21 23:02:27 -0600

    Migrated to Kotlin 1.3.61. Migrated to AndroidX. Imported BleGattCoroutines. Implemented test function.

---
+++ TaskeRGB/app/src/main/java/com/solersoft/taskergb/ble/GattHelpers.kt
@@ -0,0 +1,60 @@
+package com.solersoft.taskergb.ble
+
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGattService
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.beepiz.bluetooth.gattcoroutines.GattConnection
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.consumeEach
+
+const val GATT_HELPERS_TAG = "GattHelpers"
+
+@RequiresApi(18)
+fun deviceFor(context: Context, macAddress: String): BluetoothDevice {
+    val manager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    return manager.adapter.getRemoteDevice(macAddress)
+}
+
+@ObsoleteCoroutinesApi
+fun GattConnection.logConnectionChanges() {
+    GlobalScope.launch(Dispatchers.Main) {
+        stateChangeChannel.consumeEach {
+            Log.i(GATT_HELPERS_TAG,"Connection state changed: $it")
+        }
+    }
+}
+
+/**
+ * Connects to the device, discovers services, executes [block] and finally closes the connection.
+ */
+@RequiresApi(18)
+@UseExperimental(ObsoleteCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+suspend inline fun BluetoothDevice.useBasic(
+        connectionTimeoutInMillis: Long = 5000L,
+        block: (GattConnection, List<BluetoothGattService>) -> Unit
+) {
+    val deviceConnection = GattConnection(this)
+    try {
+        deviceConnection.logConnectionChanges()
+        withTimeout(connectionTimeoutInMillis) {
+            deviceConnection.connect()
+        }
+        Log.i(GATT_HELPERS_TAG, "Connected!")
+        val services = deviceConnection.discoverServices()
+        Log.i(GATT_HELPERS_TAG,"Services discovered!")
+        block(deviceConnection, services)
+    } catch (e: TimeoutCancellationException) {
+        Log.e(GATT_HELPERS_TAG, "Connection timed out after $connectionTimeoutInMillis milliseconds!")
+        throw e
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: Exception) {
+        Log.e(GATT_HELPERS_TAG, "Unknown BLE error", e)
+    } finally {
+        deviceConnection.close()
+        Log.i(GATT_HELPERS_TAG,"Closed!")
+    }
+}
--- TaskeRGB/app/src/main/java/com/solersoft/taskergb/tasker/rgb/Runner.kt
+++ TaskeRGB/app/src/main/java/com/solersoft/taskergb/tasker/rgb/Runner.kt
@@ -1,17 +1,23 @@
 package com.solersoft.taskergb.tasker.rgb
 
 import android.content.Context
+import android.util.Log
+import com.beepiz.blegattcoroutines.genericaccess.GenericAccess
 import com.joaomgcd.taskerpluginlibrary.action.TaskerPluginRunnerAction
 import com.joaomgcd.taskerpluginlibrary.input.TaskerInput
 import com.joaomgcd.taskerpluginlibrary.runner.TaskerPluginResult
 import com.joaomgcd.taskerpluginlibrary.runner.TaskerPluginResultSucess
 import com.solersoft.taskergb.R
+import com.solersoft.taskergb.ble.deviceFor
+import com.solersoft.taskergb.ble.useBasic
+import kotlinx.coroutines.runBlocking
 
 
 // Represents a Tasker action
 class RGBWRunner : TaskerPluginRunnerAction<RGBWInput, RGBWOutput>() {
 
     companion object {
+        val TAG = this::class.simpleName
         const val ERR_CONFIG = -1
         const val ERR_DEVICE_UNAVAILABLE = -2
         const val GATT_SERVICE = "0000FFE5-0000-1000-8000-00805F9B34FB"
@@ -21,6 +27,20 @@ class RGBWRunner : TaskerPluginRunnerAction<RGBWInput, RGBWOutput>() {
     // Add our custom plugin icon
     override val notificationProperties get() = NotificationProperties(iconResId = R.drawable.plugin)
 
+    private val defaultDeviceMacAddress = "F2:D6:43:93:70:7A"
+
+    suspend fun logNameAndAppearance(context: Context, deviceMacAddress: String = defaultDeviceMacAddress) {
+        deviceFor(context, deviceMacAddress).useBasic { device, services ->
+            services.forEach { Log.d(TAG,"Service found with UUID: ${it.uuid}") }
+            with(GenericAccess) {
+                device.readAppearance()
+                Log.d(TAG,"Device appearance: ${device.appearance}")
+                device.readDeviceName()
+                Log.d(TAG,"Device name: ${device.deviceName}")
+            }
+        }
+    }
+
     // Performs the real work of the action
     override fun run(context: Context, input: TaskerInput<RGBWInput>): TaskerPluginResult<RGBWOutput> {
 
@@ -29,8 +49,15 @@ class RGBWRunner : TaskerPluginRunnerAction<RGBWInput, RGBWOutput>() {
         var value = input.regular.value
 
         // Make sure we have valid inputs
+        /*
         require(device.isValid().success && value.isValid().success) {
             context.getString(R.string.rgbwErrConfig)
+        }*/
+
+        // This would normally be a coroutine, but Tasker doesn't support that
+        // so we have to run blocking
+        runBlocking {
+            logNameAndAppearance(context)
         }
 
         // TODO: Actually do some work!
